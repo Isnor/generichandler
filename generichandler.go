9c9d6546263b1be5f373e0e9dfad035386148580
James Isnor <isnor.james@gmail.com>
2023-11-21 23:06:49 -0400

    fix comments, doc, ToHandlerFunc signature

--- generichandler.go
+++ generichandler.go
@@ -13,6 +13,7 @@ type ErrorResponse struct {
 	Error string `json:"error"`
 }
 
+// APIEndpoint represents the function that is called when an endpoint is invoked
 type APIEndpoint[RequestType, ResponseType any] func(context.Context, *RequestType) (*ResponseType, error)
 
 var (
@@ -23,7 +24,7 @@ var (
 // an HTTPDecoder is a function that transforms an HTTP request into a concrete type
 type HTTPDecoder[RequestType any] func(*http.Request) (*RequestType, error)
 
-// an HTTPEncoder is a function that transforms a
+// an HTTPEncoder is a function that writes data to the HTTP response
 type HTTPEncoder[ResponseType any] func(http.ResponseWriter, *ResponseType) error
 
 // DefaultHTTPDecoder unmarshals JSON from the request body
@@ -42,6 +43,7 @@ func DefaultHTTPDecoder[RequestType any](request *http.Request) (*RequestType, e
 	return requestData, nil
 }
 
+// DefaultHTTPEncoder writes data as JSON to an HTTP response
 func DefaultHTTPEncoder[ResponseType any](w http.ResponseWriter, data *ResponseType) error {
 	if data != nil {
 		return json.NewEncoder(w).Encode(data)
@@ -49,63 +51,59 @@ func DefaultHTTPEncoder[ResponseType any](w http.ResponseWriter, data *ResponseT
 	return nil
 }
 
-// ToHandlerFunc returns an http.HandlerFunc that attempts to unmarshal JSON from the request body and use it
-// as an argument to the provided `endpoint` function, along with the request context. It then tries to handle
-// any errors encountered (x)or marshal the output of the function as JSON to the response body.
-// There isn't a ton of reason to export this function, and it won't be clear to anybody why this is a function
-// with 3 function arguments that returns a function with a function argument that also returns a function - actually,
-// there's almost certainly no reason to do it like that. Originally, I wanted to extend the previous `ToHandlerFunc`
-// to allow the caller control over how the request is serialized/deserialized, but still provide a clean shorthand
-// to wrap the common `APIEndpoint`s.
+// ToHandlerFunc returns an http.HandlerFunc composed of decoder, handler, and encoder that somewhat
+// resembles encoder(handler(decoder(request))). It can be used to create `http.Handler`s for endpoints
+// that require a decoder or encoder that isn't provided by this package.  Most common endpoints that
+// expect JSON on the request and response body can be wrapped by this function.
 func ToHandlerFunc[RequestType, ResponseType any](
 	decoder HTTPDecoder[RequestType],
 	endpoint APIEndpoint[RequestType, ResponseType],
 	encoder HTTPEncoder[ResponseType],
-) func(APIEndpoint[RequestType, ResponseType]) http.HandlerFunc {
+) http.HandlerFunc {
 
-	return func(a APIEndpoint[RequestType, ResponseType]) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			ctx := r.Context()
-			// deserialize the body
-			requestData := new(RequestType)
-			if r.Body != nil {
-				var err error
-				requestData, err = decoder(r)
-				if err != nil {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := r.Context()
+		// deserialize the body
+		requestData := new(RequestType)
+		if r.Body != nil {
+			var err error
+			requestData, err = decoder(r)
+			if err != nil {
+				w.WriteHeader(http.StatusBadRequest)
+				writeErrorJSON(w, err)
+				return
+			}
+
+			// if the request type has a Validate method defined
+			if req, isValidatable := (any(requestData)).(Validatable); isValidatable {
+				if err := req.Validate(ctx); err != nil {
 					w.WriteHeader(http.StatusBadRequest)
 					writeErrorJSON(w, err)
+					return
 				}
-
-				// if the request type has a Validate method defined
-				if req, isValidatable := (any(requestData)).(Validatable); isValidatable {
-					if err := req.Validate(ctx); err != nil {
-						w.WriteHeader(http.StatusBadRequest)
-						writeErrorJSON(w, err)
-						return
-					}
-				}
-			} else {
-				requestData = nil
 			}
+		} else {
+			requestData = nil
+		}
 
-			responseData, err := endpoint(ctx, requestData)
-			if err != nil {
-				writeErrorJSON(w, err)
-				return
-			}
+		responseData, err := endpoint(ctx, requestData)
+		if err != nil {
+			writeErrorJSON(w, err)
+			return
+		}
 
-			if err := encoder(w, responseData); err != nil {
-				writeErrorJSON(w, err)
-				return
-			}
+		if err := encoder(w, responseData); err != nil {
+			writeErrorJSON(w, err)
+			return
 		}
 	}
 }
 
-// DefaultJSONHandlerFunc uses the default decoder and encoder to wrap the `endpoint` in an opaque `http.HandlerFunc` that somewhat resembles
-// `encoder(handler(decoder()))`
+// DefaultJSONHandlerFunc uses the default decoder and encoder to wrap the `endpoint` returns an http.HandlerFunc
+// that attempts to unmarshal JSON from the request body, use it and the request context as arguments to the provided `endpoint`
+// function, and then write the response of that function as JSON
 func DefaultJSONHandlerFunc[RequestType, ResponseType any](endpoint APIEndpoint[RequestType, ResponseType]) http.HandlerFunc {
-	return ToHandlerFunc(DefaultHTTPDecoder[RequestType], endpoint, DefaultHTTPEncoder[ResponseType])(endpoint)
+	return ToHandlerFunc(DefaultHTTPDecoder[RequestType], endpoint, DefaultHTTPEncoder[ResponseType])
 }
 
 // Most RequestTypes are going to require some kind of validation, and it would be annoying to need to
@@ -119,5 +117,5 @@ type Validatable interface {
 // shorthand function to reduce code verbosity; writes err.Error to a JSON object on the response
 func writeErrorJSON(w http.ResponseWriter, err error) {
 	w.WriteHeader(http.StatusBadRequest)
-	json.NewEncoder(w).Encode(ErrorResponse{Error: err.Error()})
+	json.NewEncoder(w).Encode(&ErrorResponse{Error: err.Error()})
 }
